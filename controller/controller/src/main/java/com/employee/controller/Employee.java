172b4bfcb02a486a82520aea4c546239cb4d1432
khanhhai <khannhhai02012003@gmail.com>
2025-04-22 16:24:35 +0700

    message for you

---
+++ controller/controller/src/main/java/com/employee/controller/Employee.java
@@ -0,0 +1,27 @@
+package com.employee.controller;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Employee {
+    UUID id;
+    String name;
+    LocalDate dob;
+    Gender gender;
+    Double salary;
+    String phone;
+    Integer departmentId;
+}
+enum Gender {
+    MALE,
+    FEMALE,
+    OTHER;
